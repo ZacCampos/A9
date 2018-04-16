cf9820132008431a3b99289a9d3f8a34943b312c
Zac <32443586+ZacCampos@users.noreply.github.com>
2018-04-16 15:34:58 -0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * AUTHOR		: Zac Campos
+ * STUDENT ID	: 1058476
+ * ASSIGN #9	: Functions
+ * CLASS		: CS1A
+ * SECTION		: MW 8am
+ * DUE DATE		: 4/18/18
+ ******************************************************************************/
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void GetSalesInfo(int accountNum, int saleDay, int saleMonth,
+				  int saleYear, char countyCode, float salesAmount,
+				  int weight);
+
+
+#endif /* HEADER_H_ */
